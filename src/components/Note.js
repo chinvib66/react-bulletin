ea5a90b71dfc517208fa1504ebf71d6d24256ce0
chinmay <chinmayvib@gmail.com>
2018-11-05 03:50:53 +0530

    board complete f-end

---
+++ src/components/Note.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import Draggable from 'react-draggable';
+
+class Note extends React.Component{
+    constructor(props) {
+		super(props)
+		this.state = {
+			editing: false
+		}
+		this.edit = this.edit.bind(this)
+		this.remove = this.remove.bind(this)
+		this.save = this.save.bind(this)
+		this.renderForm = this.renderForm.bind(this)
+		this.renderDisplay = this.renderDisplay.bind(this)
+		this.randomBetween = this.randomBetween.bind(this)
+    }
+    componentWillMount() {
+		this.style = {
+			right: this.randomBetween(0, window.innerWidth - 150, 'px'),
+			top: this.randomBetween(0, window.innerHeight - 150, 'px'),
+		}
+    }
+    randomBetween(x, y, s) {
+		return x + Math.ceil(Math.random() * (y-x)) + s
+    }
+    componentDidUpdate() {
+		var textArea
+		if(this.state.editing) {
+			textArea = this._newText
+			textArea.focus()
+			//textArea.select()
+		}
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+		return (
+			this.props.children !== nextProps.children || this.state !== nextState
+		)
+    }
+    edit() {
+		this.setState({
+			editing: true
+		})
+	}
+
+	remove() {
+		this.props.onRemove(this.props.index)
+    }
+
+    save(e) {
+		e.preventDefault()
+		this.props.onChange(this._newText.value, this.props.index)
+		this.setState({
+			editing: false
+		})
+    }
+    renderForm() {
+		return (
+			<div className="note" style={this.style}>
+				<form onSubmit={this.save}>
+                        <textarea type="textarea" ref={input => this._newText = input}
+                                            defaultValue={this.props.children}></textarea>
+					<button id="save">S</button>
+				</form>
+			</div>
+		)
+    }
+    renderDisplay() {
+		return (
+            <Draggable>
+							<div className="note" style={this.style}>
+								<p>{this.props.children}</p>
+								<span>
+									<button onClick={this.edit} id="edit">E</button>
+									<button onClick={this.remove} id="remove">D</button>
+								</span>		
+							</div>
+            </Draggable>
+		)
+	}
+	render() {
+		return this.state.editing ? this.renderForm() : this.renderDisplay()
+	}
+}
+export default Note
