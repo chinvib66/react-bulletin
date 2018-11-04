b0369c053290b696bb7cef5b8ee9a300f4094b8d
chinmay <chinmayvib@gmail.com>
2018-11-05 04:44:34 +0530

    Updates

---
+++ precache-manifest.0e3e7b7efc37a424d0eff190927a4038.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32d33f8c1d10301e54c0",
+    "url": "/react-bulletin/static/js/runtime~main.32d33f8c.js"
+  },
+  {
+    "revision": "f67a83a8c7f60ef6e2ff",
+    "url": "/react-bulletin/static/js/main.f67a83a8.chunk.js"
+  },
+  {
+    "revision": "573bde9d9a07847043cb",
+    "url": "/react-bulletin/static/js/1.573bde9d.chunk.js"
+  },
+  {
+    "revision": "f67a83a8c7f60ef6e2ff",
+    "url": "/react-bulletin/static/css/main.440cb6ce.chunk.css"
+  },
+  {
+    "revision": "e20f11b8c6844657d8b176ebaccd517a",
+    "url": "/react-bulletin/index.html"
+  }
+];
