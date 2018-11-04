5bdb9fc10df06acd6913c4794af6e2a7cc0484f7
chinmay <chinmayvib@gmail.com>
2018-11-05 04:23:05 +0530

    build

---
+++ build/precache-manifest.086f9d66802b11f484eb58edf682e613.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bfd78e79a709549ed3c5",
+    "url": "/static/js/main.bfd78e79.chunk.js"
+  },
+  {
+    "revision": "573bde9d9a07847043cb",
+    "url": "/static/js/1.573bde9d.chunk.js"
+  },
+  {
+    "revision": "bfd78e79a709549ed3c5",
+    "url": "/static/css/main.440cb6ce.chunk.css"
+  },
+  {
+    "revision": "c48bb06819306b3669c724a595312106",
+    "url": "/index.html"
+  }
+];
